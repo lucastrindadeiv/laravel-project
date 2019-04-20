4574601fac1c32bb74b52544fdfb7eca321a7c77
Lucas Trindade <lucastrindadeiv@gmail.com>
2019-04-20 10:01:26 -0300

    Commit Inicial
    Instalação Composer e Laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php 
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller {
+
+	public function home() {
+		
+	}
+
+	// public function teste() {
+		// echo "Lucas";
+	// }
+
+
+}
+
+
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@home');
+
+Route::get('/algumacoisa', 'HomeController@teste');
+
+Route::get('/algumacoisa/{id}', 'HomeController@teste');
+
+// id = qualquer coisa que por na url vai continuar mostrando o alguma coisa
